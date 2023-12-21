565a87eca3c59748f3ccb335d3dc53593e44b9f0
Sean Allred <allred.sean@gmail.com>
2023-12-21 00:36:42 -0600

    Initial commit

---
+++ bugreport-pkg.el
@@ -0,0 +1,4 @@
+(define-package
+  "bugreport20231221"
+  "0.1"
+  "Bug report on 2023-12-21")
---
+++ bugreport.el
@@ -0,0 +1,31 @@
+;;; bugreport-20231221.el --- test package for bug report  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Sean Allred
+
+;; Author: Sean Allred <allred.sean@gmail.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This isn't the greatest package in the world; this is just a tribute
+
+;;; Code:
+
+(eval-when-compile
+  (message "(bugreport) hello from the compiler"))
+
+(provide 'bugreport)
+;;; bugreport-20231221.el ends here
